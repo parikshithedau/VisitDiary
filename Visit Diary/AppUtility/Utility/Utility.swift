a1d22ed7ea0c1362335c0695471c48512fef2277
parikshithedau <parikshit.hedau@gmail.com>
2021-03-25 21:16:51 +0530

    branch 2 merged

--- Visit Diary/AppUtility/Utility/Utility.swift
+++ Visit Diary/AppUtility/Utility/Utility.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class Utility: NSObject {
 
+    // This comment is added in branch2
     func rgb(_ red:CGFloat, _ green:CGFloat, _ blue:CGFloat, _ alpha:CGFloat) -> UIColor {
         
         let color = UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: alpha)
